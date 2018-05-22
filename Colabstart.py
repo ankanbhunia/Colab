860d842b53774eb23eca283c99d3c33b15374fcf
ankanbhunia <ankankumarbhunia@gmail.com>
2018-05-22 12:59:27 +0530

    Update Colabstart.py

--- Colabstart.py
+++ Colabstart.py
@@ -10,31 +10,33 @@ def localtunnel(domain,port=6007):
 	print ('http://'+domain + '.localtunnel.me')
 	
 def load(port=6007):
-	cmd = ['apt-get install -y -qq software-properties-common python-software-properties module-init-tools',
-	'add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null',
-	'apt-get update -qq 2>&1 > /dev/null',
-	'apt-get -y install -qq google-drive-ocamlfuse fuse',
-	'apt-get install unrar',
-	'apt-get install ruby-full',
-	'pip install --upgrade jupyter',
-	'pip install jupyter-tensorboard',
-	'pip install jupyter_contrib_nbextensions',
-	'pip install jupyter_nbextensions_configurator',
-	'apt-get -qq install -y libsm6 libxext6 && pip install -q -U opencv-python',
-	'pip install -q keras',
-	'pip install jupyterlab',
-	'jupyter notebook --generate-config',
-	"echo \"c.NotebookApp.token = u''\" >> ~/.jupyter/jupyter_notebook_config.py",
-	"echo \"c.NotebookApp.notebook_dir = u''\" >> ~/.jupyter/jupyter_notebook_config.py",
-	'./ngrok authtoken 5vhWvAzJGtsJbnVp4V5di_6KNVTN8BpHMqKYyAaFFXQ',
-	'unzip Colab/.gdfuse.zip',
-	'mkdir -p drive',
-	'google-drive-ocamlfuse drive',
-	'npm install -g localtunnel',
-	'wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip',
-	'unzip ngrok-stable-linux-amd64.zip']
-
-	for i in cmd:
-	  get_ipython().getoutput(i)
+	cmd = [['apt-get install -y -qq software-properties-common python-software-properties module-init-tools',True],
+	['add-apt-repository -y ppa:alessandro-strada/ppa 2>&1 > /dev/null',True],
+	['apt-get update -qq 2>&1 > /dev/null',True],
+	['apt-get -y install -qq google-drive-ocamlfuse fuse',True],
+	['apt-get install unrar',True],
+	['apt-get install ruby-full',True],
+	['pip install --upgrade jupyter',True],
+	['pip install jupyter-tensorboard',True],
+	['pip install jupyter_contrib_nbextensions',True],
+	['pip install jupyter_nbextensions_configurator',True],
+	['apt-get -qq install -y libsm6 libxext6 && pip install -q -U opencv-python',True],
+	['pip install -q keras',True],
+	['pip install jupyterlab',True],
+	['jupyter notebook --generate-config',True],
+	["echo \"c.NotebookApp.token = u''\" >> ~/.jupyter/jupyter_notebook_config.py",True],
+	["echo \"c.NotebookApp.notebook_dir = u''\" >> ~/.jupyter/jupyter_notebook_config.py",True],
+	['./ngrok authtoken 5vhWvAzJGtsJbnVp4V5di_6KNVTN8BpHMqKYyAaFFXQ',True],
+	['unzip gdfuse.zip',not os.path.isdir("/content/.gdfuse")],
+	['mkdir -p drive',not os.path.isdir("drive")],
+	['google-drive-ocamlfuse drive', not os.path.isdir("drive")],
+	['npm install -g localtunnel'],
+	['wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'],
+	['unzip ngrok-stable-linux-amd64.zip'],not os.path.isdir("ngrok")]
+  
+  for i in cmd:
+    if i[1]:
+      sys.stdout.write(' '.join(get_ipython().getoutput(i[0]))) ; sys.stdout.flush()
+    
 	if len(get_ipython().getoutput('jupyter notebook list'))==2:
 	  get_ipython().system_raw("jupyter notebook --port "+str(port)+" &")
