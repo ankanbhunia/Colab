91b3befbb75fb0fe3a0c362d1ab552ae9dd45fe5
ankanbhunia <ankankumarbhunia@gmail.com>
2018-05-22 15:20:58 +0530

    Delete task.py

--- task.py
+++
@@ -1,4 +0,0 @@
-import pandas
-import numpy as np
-dd = [i.split(' ') for i in get_ipython().getoutput("ps -ef | grep python| grep -v grep | awk '{print $2, $7, $8}'")]
-pandas.DataFrame(dd,np.arange(1,len(dd)+1),['PID','time','process'])
