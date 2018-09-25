8b8151236380d4d276074b02346a13ec3554de16
ankanbhunia <ankankumarbhunia@gmail.com>
2018-09-25 22:01:41 +0530

    Update Colabstart.py

--- Colabstart.py
+++ Colabstart.py
@@ -69,47 +69,12 @@ import sys
 
 
 
-def ModConfig(Variable, Setting):
-    """
-    Modify Config file variable with new setting
-    """
-
-    File = '/root/.gdfuse/default/state'
-    VarFound = False
-    AlreadySet = False
-    V=str(Variable)
-    S=str(Setting)
-    # use quotes if setting has spaces #
-    if ' ' in S:
-        S = '"%s"' % S
-
-    for line in fileinput.input(File, inplace = 1):
-        # process lines that look like config settings #
-        if not line.lstrip(' ').startswith('#') and '=' in line:
-            _infile_var = str(line.split('=')[0].rstrip(' '))
-            _infile_set = str(line.split('=')[1].lstrip(' ').rstrip())
-            # only change the first matching occurrence #
-            if VarFound == False and _infile_var.rstrip(' ') == V:
-                VarFound = True
-                # don't change it if it is already set #
-                if _infile_set.lstrip(' ') == S:
-                    AlreadySet = True
-                else:
-                    line = "%s = %s\n" % (V, S)
-
-        sys.stdout.write(line)
-
-
-    # Append the variable if it wasn't found #
-    if not VarFound:
-        print( "Variable '%s' not found.  Adding it to %s" % (V, File))
-        with open(File, "a") as f:
-            f.write("%s = %s\n" % (V, S))
-    elif AlreadySet == True:
-        print ("Variable '%s' unchanged" % (V))
-    else:
-        print ("Variable '%s' modified to '%s'" % (V, S))
-
-    return
-
-	     
+def changevalue(t):
+  
+  txt = ''
+  for i in t.keys():
+    txt = txt + i+'='+t[i]+'\n'
+  
+  with open('/root/.gdfuse/default/state','w') as f:
+    f.write(txt)
+  
