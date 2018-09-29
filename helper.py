df5781169fd677b27fd1eb6b6e06252f93809493
ankanbhunia <ankankumarbhunia@gmail.com>
2018-09-29 23:40:23 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,56 +1,66 @@
-from IPython.display import HTML
+from __future__ import print_function
 
-import time
-import ipywidgets as wg
-from IPython.display import display
 import os
-
-
-def helper_():
+import pandas
+import numpy as np
+import threading
+def Background_(function):
     
+    t = threading.Thread(target=function)
+    t.daemon = True
+    t.start()
+    
+def show_():
+    while True:
+        time.sleep(2)
+        print(get_ipython().getoutput('gpustat' )[1], end='\r')
+        
+Background_(show_)
 
+    
+def Show_running_process():
+    print ('Running Programs : ')
+    print(task('python'))
+def Kill(PID):
+    print(get_ipython().getoutput('kill '+str(PID)))
+def Reset():    
+    get_ipython().getoutput('kill -9 -1')
+def Download_from_Drive(ID, Name):
+    get_ipython().system_raw('pip install googledrivedownloader')
+    from google_drive_downloader import GoogleDriveDownloader as gdd
+    gdd.download_file_from_google_drive(file_id=ID,
+                                        dest_path='/content/'+Name,
+                                        unzip=False)
+def task(pross):
+    dd = [i.split(' ') for i in get_ipython().getoutput("ps -eF | grep "+pross+"| grep -v grep | awk '{print  $2,  $6, $10, $11}'")]
+    return pandas.DataFrame(dd,np.arange(1,len(dd)+1),['PID','memory','time','process'])
+def Extract(path):
+    import os
+    pp = path.split('/')
 
-    class printer():
-        """
-        Print things to stdout on one line dynamically
-        """
+    pp1 = '/'.join(pp[:-1])
 
-        def __init__(self,st):
-            print (time.strftime('%Y-%m-%d %H:%M %p', time.localtime())+ ' >> ' + st)
+    if os.path.isdir(path):
+      hh= os.listdir(path)
+      for i in hh:
+        print (i)
+    elif os.path.isdir(pp1):
+      kk = ([i for i in os.listdir(pp1) if pp[-1] in i])
+      for i in kk:
+        zz = i.split(pp[-1])
+        print(zz[0] + '\x1b[1;37;40m' + pp[-1] + '\x1b[0m' + zz[1])
+    path1  = path.split(' ')
+    path1 = '\ '.join(path1)   
+    if os.path.isfile(path):
 
-    datadict = {i:ind+1 for ind,i in enumerate(os.listdir('drive/datasets/'))}
-    dddataset = wg.Dropdown(options=datadict, value=1,
-                      description='Dataset',
-                      button_style='')
-    butt2 = wg.Button(description='Extract')
-    inv_map = {v: k for k, v in datadict.items()}
-    def data_click(event):
-        dpath = 'drive/datasets/' + inv_map[dddataset.value]
-        if os.path.isfile(dpath):
-          if dpath.endswith('zip'):
-            get_ipython().getoutput('unzip '+ dpath)
-            printer ('Dataset successfully imported')
+      if path.endswith('zip'):
+        print ('Extracting ' + path)
+        get_ipython().system_raw('unzip '+ path1)
 
-          if dpath.endswith('rar'):
-            get_ipython().getoutput('unrar x  '+ dpath)
-            printer ('Dataset successfully imported')
+      if path.endswith('rar'):
+        print ('Extracting ' + path)
+        get_ipython().system_raw('unrar x  '+ path1)
 
-          if dpath.endswith('tar.gz'):
-            get_ipython().getoutput('tar xvf  '+ dpath)
-            printer ('Dataset successfully imported')
-    butt2.on_click(data_click)   
-    ss3 = wg.HBox([dddataset,butt2])
-    display(wg.VBox([ss3]))
-p = """<script>
-        code_show=true; 
-        function code_toggle() {
-         if (code_show){
-         $('div.input').hide();
-         } else {
-         $('div.input').show();
-         }
-         code_show = !code_show
-        } 
-        $( document ).ready(code_toggle);
-        </script>
-        """
+      if path.endswith('tar.gz'):
+        print ('Extracting ' + path)
+        get_ipython().system_raw('tar -xvf '+ path1)
