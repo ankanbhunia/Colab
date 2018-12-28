62420a852cff9596bdee4d0301ddb01e18e5b809
ankanbhunia <ankankumarbhunia@gmail.com>
2018-12-28 18:54:41 +0530

    Update Colabstart.py

--- Colabstart.py
+++ Colabstart.py
@@ -45,7 +45,7 @@ def load(port=6007,show_result=False):
         ['apt-get install -f',True],
 	['apt-get -y install -qq fuse', True],
 	['unzip gdfuse.zip -d /', not os.path.isdir("/root/.gdfuse")],
-	['cp /content/cldrh.conf /root/.config/rclone/rclone.conf', True],
+	['unzip rclone.zip -d /', True],
 	['mkdir -p database', not os.path.isdir("database")],
 	['sleep 3', True],
 	['rclone --vfs-cache-mode writes mount onedrive: database &', True]]
