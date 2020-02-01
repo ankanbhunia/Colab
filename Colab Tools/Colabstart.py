df261379fe42f7f89289197165eb0ad60a4f99ff
Ankan Kumar Bhunia <ankankumarbhunia@gmail.com>
2020-02-01 13:53:21 +0530

    Update Colabstart.py

--- Colab Tools/Colabstart.py
+++ Colab Tools/Colabstart.py
@@ -10,22 +10,23 @@ import pickle
 import sys
 import os
 get_ipython().system_raw('pip3 install PySimpleGUI')
-import PySimpleGUI as sg
-folder_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABnUlEQVQ4y8WSv2rUQRSFv7vZgJFFsQg2EkWb4AvEJ8hqKVilSmFn3iNvIAp21oIW9haihBRKiqwElMVsIJjNrprsOr/5dyzml3UhEQIWHhjmcpn7zblw4B9lJ8Xag9mlmQb3AJzX3tOX8Tngzg349q7t5xcfzpKGhOFHnjx+9qLTzW8wsmFTL2Gzk7Y2O/k9kCbtwUZbV+Zvo8Md3PALrjoiqsKSR9ljpAJpwOsNtlfXfRvoNU8Arr/NsVo0ry5z4dZN5hoGqEzYDChBOoKwS/vSq0XW3y5NAI/uN1cvLqzQur4MCpBGEEd1PQDfQ74HYR+LfeQOAOYAmgAmbly+dgfid5CHPIKqC74L8RDyGPIYy7+QQjFWa7ICsQ8SpB/IfcJSDVMAJUwJkYDMNOEPIBxA/gnuMyYPijXAI3lMse7FGnIKsIuqrxgRSeXOoYZUCI8pIKW/OHA7kD2YYcpAKgM5ABXk4qSsdJaDOMCsgTIYAlL5TQFTyUIZDmev0N/bnwqnylEBQS45UKnHx/lUlFvA3fo+jwR8ALb47/oNma38cuqiJ9AAAAAASUVORK5CYII='
-file_icon = b'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsSAAALEgHS3X78AAABU0lEQVQ4y52TzStEURiHn/ecc6XG54JSdlMkNhYWsiILS0lsJaUsLW2Mv8CfIDtr2VtbY4GUEvmIZnKbZsY977Uwt2HcyW1+dTZvt6fn9557BGB+aaNQKBR2ifkbgWR+cX13ubO1svz++niVTA1ArDHDg91UahHFsMxbKWycYsjze4muTsP64vT43v7hSf/A0FgdjQPQWAmco68nB+T+SFSqNUQgcIbN1bn8Z3RwvL22MAvcu8TACFgrpMVZ4aUYcn77BMDkxGgemAGOHIBXxRjBWZMKoCPA2h6qEUSRR2MF6GxUUMUaIUgBCNTnAcm3H2G5YQfgvccYIXAtDH7FoKq/AaqKlbrBj2trFVXfBPAea4SOIIsBeN9kkCwxsNkAqRWy7+B7Z00G3xVc2wZeMSI4S7sVYkSk5Z/4PyBWROqvox3A28PN2cjUwinQC9QyckKALxj4kv2auK0xAAAAAElFTkSuQmCC'
 
-treedata = sg.TreeData()
-def add_files_in_folder(parent, dirname, savepath,c):
-    c = c + 1 
+
+def add_files_in_folder(treedata, parent, dirname, savepath,c):
+
+    c += 1
+    
     files = os.listdir(dirname)
     for f in files:
         fullname = os.path.join(dirname, f)
         if os.path.isdir(fullname):            # if it's a folder, add folder and recurse
             treedata.Insert(parent, fullname, f, values=[], icon=folder_icon)
-            if c<4: 
+            #limit = sys.getsizeof(treedata.tree_dict)
+            if c<5: 
               
-              add_files_in_folder(fullname, fullname,savepath,c )
-        else:
+              add_files_in_folder(treedata,fullname, fullname, savepath,c)
+            
+        elif fullname.endswith('zip') or fullname.endswith('rar') or fullname.endswith('7z') or fullname.endswith('gz'):
             treedata.Insert(parent, fullname, f, values=[
                             "{0:.2f}".format(os.stat(fullname).st_size/(1024*1024))], icon=file_icon)
 
@@ -33,6 +34,8 @@ def add_files_in_folder(parent, dirname, savepath,c):
     with open(savepath, 'wb') as treefile:
 
       pickle.dump(treedata, treefile)
+    
+      
 def task(pross):
 	dd = [i.split(' ') for i in get_ipython().getoutput("ps -eF | grep "+pross+"| grep -v grep | awk '{print  $2,  $6, $10, $11}'")]
 	return pandas.DataFrame(dd,np.arange(1,len(dd)+1),['PID','memory','time','process'])
